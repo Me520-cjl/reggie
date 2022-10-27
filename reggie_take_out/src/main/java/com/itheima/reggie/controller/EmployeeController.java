572258b0bbf4a424885317d7d07215c5b84dd2b3
Me520 <2650208828@qq.com>
2022-10-28 02:10:03 +0800

    完成了添加员工功能

--- reggie_take_out/src/main/java/com/itheima/reggie/controller/EmployeeController.java
+++ reggie_take_out/src/main/java/com/itheima/reggie/controller/EmployeeController.java
@@ -11,8 +11,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sun.print.PSPrinterJob;
 
 import javax.servlet.http.HttpServletRequest;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
 
 @Slf4j
 @RestController
@@ -61,11 +64,41 @@ public class EmployeeController {
 
     /**
      * 员工退出
+     * @param request
+     * @return
      */
-//    @PostMapping("/logout")
-//    public R<string> logout(HttpServletRequest){
-//        //清理Session中保存的当前员工的id
-//        request.getSession().removeAttribute("employee");
-//        return R.succss("退出成功");
-//    }
+    @PostMapping("/logout")
+    public R<String> logout(HttpServletRequest request){
+        //清理Session中保存的当前登录员工的id
+        request.getSession().removeAttribute("employee");
+        return R.success("退出成功");
+    }
+
+    /**
+     * 新增员工
+     * @param request
+     * @param employee
+     * @return
+     */
+    @PostMapping
+    public  R<String> save(HttpServletRequest request, @RequestBody Employee employee){
+
+        //设置初始密码123456，需要进行md5加密处理
+        employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
+
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+
+        //获取当前登录用户的id
+        Long empId = (Long) request.getSession().getAttribute("employee");
+
+        employee.setCreateUser(empId);
+        employee.setUpdateUser(empId);
+
+        employeeService.save(employee);
+        log.info("新增员工，员工信息，{}",employee.toString());
+        return R.success("新增员工成功");
+
+    }
+
 }
---
+++ reggie_take_out/src/main/java/com/itheima/reggie/filter/LoginCheckFilter.java
@@ -0,0 +1,79 @@
+package com.itheima.reggie.filter;
+
+import com.alibaba.fastjson.JSON;
+import com.itheima.reggie.common.R;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.AntPathMatcher;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@Slf4j
+@WebFilter(filterName = "loginCheckFilter",urlPatterns = "/*")
+public class LoginCheckFilter implements Filter {
+    //路径匹配器，支持通配符
+    public static final AntPathMatcher PATH_MATCHER = new AntPathMatcher();
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+//        **过滤器具体的处理逻辑如下：**
+
+//        A. 获取本次请求的URI
+        String requestURL = request.getRequestURI();
+        log.info("拦截到请求：{}",request.getRequestURI());
+        //定义不需要处理的请求路径
+        String[] urls = new String[]{
+                "/employee/login",
+                "/employee/logout",
+                "/backend/**",
+                "/front/**"
+        };
+
+
+//        B. 判断本次请求, 是否需要登录, 才可以访问
+        boolean check = check(urls,requestURL);
+
+
+//        C. 如果不需要，则直接放行
+        if (check){
+            log.info("本次请求{}不需要放行",requestURL);
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+
+//        D. 判断登录状态，如果已登录，则直接放行
+        if (request.getSession().getAttribute("employee")!=null){
+            log.info("用户已登录，用户id为：{}",request.getSession().getAttribute("employee"));
+            filterChain.doFilter(request,response);
+            return;
+        }
+//
+//        E. 如果未登录, 则返回未登录结果
+        log.info("用户未登录");
+        response.getWriter().write(JSON.toJSONString(R.error("NOTLOGIN")));
+    }
+
+    /**
+     * 路径匹配，检查本次请求是否需要放行
+     * @param urls
+     * @param requestURL
+     * @return
+     */
+    public boolean check(String[] urls,String requestURL){
+        for (String url:urls){
+            boolean match = PATH_MATCHER.match(url,requestURL);
+            if (match){
+                return true;
+            }
+        }
+        return false;
+    }
+}
